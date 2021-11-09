da7fbffa985dd25a0e5bc1c64c932473b100c8f7
OlgaNV <93245074+OlgaNV@users.noreply.github.com>
2021-11-09 16:18:45 +0500

    lesson6.dog.java

--- exercise 1/src/main/java/ru/geekbrains/lesson6/dog.java
+++ exercise 1/src/main/java/ru/geekbrains/lesson6/dog.java
@@ -1,7 +1,7 @@
 package ru.geekbrains.lesson6;
 
     public class dog {
-        public static class dog extends animals { //Не пойму что не так?
+        public static class dog extends animals {  //Не пойму что не так?
             private int runMaxDistance;
             private int swimMaxDistance;
             private float jumpMaxDistance;
