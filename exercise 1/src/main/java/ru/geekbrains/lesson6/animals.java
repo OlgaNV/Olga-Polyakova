732949380c08e02c7766e0553869c791a85002f7
OlgaNV <93245074+OlgaNV@users.noreply.github.com>
2021-11-09 16:27:54 +0500

    Update animals.java

--- exercise 1/src/main/java/ru/geekbrains/lesson6/animals.java
+++ exercise 1/src/main/java/ru/geekbrains/lesson6/animals.java
@@ -18,14 +18,12 @@ public abstract class animals {
     public animals(int runDistance, int swimDistance, float jumpHeight) {
     }
 
-
     public void Animal(int runMaxDistance, int swimMaxDistance, float jumpMaxDistance) {
 
         this.runMaxDistance = runMaxDistance;
         this.swimMaxDistance = swimMaxDistance;
         this.jumpMaxDistance = jumpMaxDistance;
     }
-
 
     public void run(int runDistance) {
         System.out.println("run: " + (runDistance <= runMaxDistance));
